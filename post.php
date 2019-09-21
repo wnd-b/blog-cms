e004b5b98577e88d8d0b8b64a9fa5f200bf580c7
Wendy Bruny <wendybruny@Wendys-Air.fios-router.home>
2019-09-21 18:17:03 -0400

    fix more links

--- post.php
+++ post.php
@@ -1,9 +1,10 @@
 <header>Hello World!</header>
 
 <nav>
-        <a href="/html/">HTML</a> |
+<a href="/html/">HTML</a> |
         <a href="/css/">CSS</a> |
-        
+        <a href="/blog-cms/post.php">POST</a> |
+        <a href="/blog-cms/post-submission.php">POST-Submission</a> |
       </nav>
 
       <main>
